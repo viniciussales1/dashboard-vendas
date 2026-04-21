24933a065572b53831989d19dcf47fbbf3e15b33
viniciussales1 <viniciussalesx100@gmail.com>
2026-04-20 22:24:41 -0300

    Update relatorio.py

--- relatorio.py
+++ relatorio.py
@@ -1,6 +1,12 @@
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle
+from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet
 from io import BytesIO
+from datetime import datetime
+
+elementos.append(
+    Paragraph(f"Data do relatório: {datetime.now().strftime('%d/%m/%Y')}", styles["Normal"])
+)
 
 
 def gerar_pdf(total_vendido, total_faturado, top_produtos):
@@ -11,20 +17,46 @@ def gerar_pdf(total_vendido, total_faturado, top_produtos):
 
     elementos = []
 
-    elementos.append(Paragraph("Relatório de Vendas", styles["Title"]))
+    # TÍTULO
+    elementos.append(Paragraph("<b>📊 RELATÓRIO DE VENDAS</b>", styles["Title"]))
     elementos.append(Spacer(1, 12))
 
-    elementos.append(Paragraph(f"Total vendido: {total_vendido}", styles["Normal"]))
-    elementos.append(Paragraph(f"Faturamento total: R$ {total_faturado:,.2f}", styles["Normal"]))
+    # RESUMO
+    elementos.append(Paragraph(f"<b>Total vendido:</b> {total_vendido}", styles["Normal"]))
+    elementos.append(Paragraph(f"<b>Faturamento total:</b> R$ {total_faturado:,.2f}", styles["Normal"]))
+    elementos.append(Spacer(1, 20))
 
-    elementos.append(Spacer(1, 12))
-    elementos.append(Paragraph("Produtos mais vendidos:", styles["Heading2"]))
+    # TABELA
+    dados = [["Produto", "Quantidade"]]
 
-    for i, row in top_produtos.head(5).iterrows():
-        elementos.append(
-            
-            Paragraph(f"{row['produto']} - {row['quantidade']}", styles["Normal"])
-        )
+    for _, row in top_produtos.head(10).iterrows():
+        dados.append([row["produto"], row["quantidade"]])
+
+    tabela = Table(dados)
+
+    tabela.setStyle(TableStyle([
+        ("BACKGROUND", (0, 0), (-1, 0), colors.darkblue),
+        ("TEXTCOLOR", (0, 0), (-1, 0), colors.white),
+
+        ("ALIGN", (0, 0), (-1, -1), "CENTER"),
+        ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
+
+        ("BOTTOMPADDING", (0, 0), (-1, 0), 10),
+        ("TOPPADDING", (0, 0), (-1, 0), 10),
+
+        ("BACKGROUND", (0, 1), (-1, -1), colors.whitesmoke),
+
+        ("GRID", (0, 0), (-1, -1), 1, colors.grey)
+    ]))
+
+    elementos.append(Paragraph("<b>Top produtos mais vendidos</b>", styles["Heading2"]))
+    elementos.append(Spacer(1, 10))
+    elementos.append(tabela)
+
+    elementos.append(Spacer(1, 20))
+
+    # RODAPÉ
+    elementos.append(Paragraph("Relatório gerado automaticamente pelo sistema.", styles["Italic"]))
 
     doc.build(elementos)
 

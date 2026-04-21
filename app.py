9b087bd7db10ba7037ba4b4668bf2a24cad19a52
viniciussales1 <viniciussalesx100@gmail.com>
2026-04-20 21:29:49 -0300

    Refactor app.py to include CSS loading and cleanup

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Análise Preditiva + Análise Descritiva", layout="wide")
 
+def carregar_css(nome_arquivo):
+    with open(nome_arquivo, "r", encoding="utf-8") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+carregar_css("style.css")
+
 # -----------------------------
 # CONFIGURAÇÃO DE USUÁRIO
 # -----------------------------

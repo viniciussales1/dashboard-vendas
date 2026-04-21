ce0301cd5566238dcaab269c74c4c303e286672c
viniciussales1 <viniciussalesx100@gmail.com>
2026-04-20 22:25:49 -0300

    Update relatorio.py

--- relatorio.py
+++ relatorio.py
@@ -4,21 +4,25 @@ from reportlab.lib.styles import getSampleStyleSheet
 from io import BytesIO
 from datetime import datetime
 
-elementos.append(
-    Paragraph(f"Data do relatório: {datetime.now().strftime('%d/%m/%Y')}", styles["Normal"])
-)
-
 
 def gerar_pdf(total_vendido, total_faturado, top_produtos):
     buffer = BytesIO()
 
     doc = SimpleDocTemplate(buffer)
     styles = getSampleStyleSheet()
-
     elementos = []
 
     # TÍTULO
-    elementos.append(Paragraph("<b>📊 RELATÓRIO DE VENDAS</b>", styles["Title"]))
+    elementos.append(Paragraph("<b>RELATÓRIO DE VENDAS</b>", styles["Title"]))
+    elementos.append(Spacer(1, 12))
+
+    # DATA
+    elementos.append(
+        Paragraph(
+            f"Data do relatório: {datetime.now().strftime('%d/%m/%Y')}",
+            styles["Normal"]
+        )
+    )
     elementos.append(Spacer(1, 12))
 
     # RESUMO
@@ -37,26 +41,23 @@ def gerar_pdf(total_vendido, total_faturado, top_produtos):
     tabela.setStyle(TableStyle([
         ("BACKGROUND", (0, 0), (-1, 0), colors.darkblue),
         ("TEXTCOLOR", (0, 0), (-1, 0), colors.white),
-
         ("ALIGN", (0, 0), (-1, -1), "CENTER"),
         ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
-
         ("BOTTOMPADDING", (0, 0), (-1, 0), 10),
         ("TOPPADDING", (0, 0), (-1, 0), 10),
-
         ("BACKGROUND", (0, 1), (-1, -1), colors.whitesmoke),
-
         ("GRID", (0, 0), (-1, -1), 1, colors.grey)
     ]))
 
     elementos.append(Paragraph("<b>Top produtos mais vendidos</b>", styles["Heading2"]))
     elementos.append(Spacer(1, 10))
     elementos.append(tabela)
-
     elementos.append(Spacer(1, 20))
 
     # RODAPÉ
-    elementos.append(Paragraph("Relatório gerado automaticamente pelo sistema.", styles["Italic"]))
+    elementos.append(
+        Paragraph("Relatório gerado automaticamente pelo sistema.", styles["Italic"])
+    )
 
     doc.build(elementos)
 
